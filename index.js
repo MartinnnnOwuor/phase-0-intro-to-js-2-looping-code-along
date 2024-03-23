e84c1a2746dee81551654fbbce4fbe0bf87462c0
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-03-23 15:54:13 +0300

    add loop.js file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", 'Aki'];
+const string = ['surprise'];
+
+function writeCards (names, string){
+
+    let messages = [];
+    for (let j = 0; j < names.length; j++){
+        let message = `Thank you, ${names[j]}, for the wonderful ${string} gift!`;
+        messages.push (message);
+        debugger;
+    } 
+    return messages;
+}
+
+
+function countDown (interger){
+
+    while (interger >=0){
+        console.log(interger--)
+    }
+}
